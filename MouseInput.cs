b5d52dde8f4350dd0215d73775194cdbf479a81a
aforarpitkumar <80710050+aforarpitkumar@users.noreply.github.com>
2021-10-26 22:13:06 +0530

    Create MouseInput.cs

---
+++ MouseInput.cs
@@ -0,0 +1,4 @@
+bool isButtonDown;
+isButtonDown = Input.GetMouseButtonDown(0); //left mouse button
+isButtonDown = Input.GetMouseButtonDown(1); //right mouse button
+isButtonDown = Input.GetMouseButtonDown(3); //center mouse button
