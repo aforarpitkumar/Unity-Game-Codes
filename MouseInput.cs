93c4b2bed9516fc690f550281e44112b5739e387
aforarpitkumar <80710050+aforarpitkumar@users.noreply.github.com>
2021-10-26 22:16:47 +0530

    Update MouseInput.cs

--- MouseInput.cs
+++ MouseInput.cs
@@ -2,3 +2,16 @@ bool isButtonDown;
 isButtonDown = Input.GetMouseButtonDown(0); //left mouse button
 isButtonDown = Input.GetMouseButtonDown(1); //right mouse button
 isButtonDown = Input.GetMouseButtonDown(3); //center mouse button
+
+float value;
+value = Input.GetAxis("Mouse X"); //x axis movement
+value = Input.GetAxis("Mouse Y"); //y axis movement
+
+/* 
+Unlike button presses, the mouse movement is measured by the amount the mouse has moved since the
+last frame only. Basically, holding a key will cause a value to increase until it maxes out at –1 or 1
+(depending on whether is it positive or negative). The mouse movement, however, will generally
+have smaller numbers because it is measured and reset every frame.
+
+*/
+
