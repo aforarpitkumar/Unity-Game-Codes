87ffd02e78c731f5c5f34e5ddbfd646b31601814
aforarpitkumar <80710050+aforarpitkumar@users.noreply.github.com>
2021-10-26 23:28:19 +0530

    Update Use_Of_Tags_Unity.cs

--- Use_Of_Tags_Unity.cs
+++ Use_Of_Tags_Unity.cs
@@ -15,3 +15,30 @@ public class SomeClassScript : MonoBehaviour {
     target = GameObject.FindWithTag("TargetCube");  // TargetCube is the tag given by you in the inspector
   }
 }
+
+/*
+Modifying Object Components
+Once you have a reference to another object, working with the components of that object is almost
+100% exactly the same. The only difference is that now, instead of simply writing the component
+name, you need to write the object variable and a period in front of it:
+*/
+
+//This accesses the local component, not what you want
+transform.Translate(0, 0, 0);
+//This accesses the target object, what you want
+targetObject.transform.Translate(0, 0, 0);
+
+
+//This is the game object you want to access
+public GameObject target;
+//This is here for reference
+  void Start() {
+    target = GameObject.Find("Cube");
+  }
+  
+  void Update() {
+    target.transform.Translate(.05f, 0f, 0f);
+    target.transform.Rotate(0f, 0f, 1f);
+    target.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+  }
+}
