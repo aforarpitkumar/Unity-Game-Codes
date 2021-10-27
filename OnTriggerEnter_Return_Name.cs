4e729e982cf29fe34455d4eaf2c80cb05dd8daa3
aforarpitkumar <80710050+aforarpitkumar@users.noreply.github.com>
2021-10-27 15:24:51 +0530

    Update OnTriggerEnter_Return_Name.cs

--- OnTriggerEnter_Return_Name.cs
+++ OnTriggerEnter_Return_Name.cs
@@ -1,4 +1,20 @@
+/*
+
+For instance, if you want to modify the preceding code to write the name of the
+object that enters the trigger to the console, you could write the following
+
+*/
+
 void OnTriggerEnter(Collider other)
 {
 print(other.gameObject.name + " has entered the trigger");
+}
+
+/*
+You could even go so far as to destroy the object entering the trigger with some code like this:
+*/
+
+void OnTriggerEnter(Collider other)
+{
+Destroy(other.gameObject);
 }
